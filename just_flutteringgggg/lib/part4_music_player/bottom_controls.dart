5d5497e83f6d2d1a4a5374113e5c8d4003b2efe5
d4g4 <daksh.gargas@peoplestrong.com>
2018-11-29 11:59:41 +0530

    Completed MusicPlayer

--- just_flutteringgggg/lib/part4_music_player/bottom_controls.dart
+++ just_flutteringgggg/lib/part4_music_player/bottom_controls.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'colors.dart';
+import 'package:fluttery_audio/fluttery_audio.dart';
+import 'package:just_flutteringgggg/part4_music_player/songs.dart';
 
 class BottomControls extends StatelessWidget {
   const BottomControls({
@@ -16,31 +18,36 @@ class BottomControls extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.only(top: 40.0, bottom: 50.0),
             child: Column(children: <Widget>[
-              RichText(
-                text: TextSpan(
-                  text: '' /*Unstyled text */,
-                  children: [
-                    TextSpan(
-                      text: 'Song Title\n',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 4.0,
-                        height: 1.5,
-                      ),
+              AudioPlaylistComponent(
+                playlistBuilder: (context, playlist, child) {
+                  DemoSong song = demoPlaylist.songs[playlist.activeIndex];
+                  return RichText(
+                    text: TextSpan(
+                      text: '' /*Unstyled text */,
+                      children: [
+                        TextSpan(
+                          text: '${song.songTitle}\n',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 4.0,
+                            height: 1.5,
+                          ),
+                        ),
+                        TextSpan(
+                          text: song.artist,
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.75),
+                            fontSize: 12.0,
+                            letterSpacing: 3.0,
+                            height: 1.5,
+                          ),
+                        )
+                      ],
                     ),
-                    TextSpan(
-                      text: 'Artist Name',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.75),
-                        fontSize: 12.0,
-                        letterSpacing: 3.0,
-                        height: 1.5,
-                      ),
-                    )
-                  ],
-                ),
+                  );
+                },
               ),
               Padding(
                   padding: const EdgeInsets.only(top: 40.0),
@@ -65,22 +72,42 @@ class PlayPauseButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RawMaterialButton(
-      shape: CircleBorder(),
-      fillColor: Colors.white,
-      splashColor: lightAccentColor,
-      highlightColor: lightAccentColor,
-      elevation: 20.0,
-      highlightElevation: 5.0,
-      onPressed: () {},
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Icon(
-          Icons.play_arrow,
-          color: darkAccentColor,
-          size: 35.0,
-        ),
-      ),
+    return AudioComponent /**Widget that looks up the tree for Audio Widget and it can report the state of Audio Widget */ (
+      updateMe: [
+        WatchableAudioProperties.audioPlayerState,
+      ],
+      playerBuilder: (BuildContext context, AudioPlayer player, Widget child) {
+        IconData icon = Icons.music_note;
+        Function onPressed;
+        Color buttonColor = lightAccentColor;
+        if (player.state == AudioPlayerState.playing) {
+          icon = Icons.pause;
+          onPressed = player.pause;
+          buttonColor = Colors.white;
+        } else if (player.state == AudioPlayerState.paused ||
+            player.state == AudioPlayerState.completed) {
+          icon = Icons.play_arrow;
+          onPressed = player.play;
+          buttonColor = Colors.white;
+        }
+        return RawMaterialButton(
+          shape: CircleBorder(),
+          fillColor: buttonColor,
+          splashColor: lightAccentColor,
+          highlightColor: lightAccentColor,
+          elevation: 20.0,
+          highlightElevation: 5.0,
+          onPressed: onPressed,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              icon,
+              color: darkAccentColor,
+              size: 35.0,
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -92,15 +119,19 @@ class PreviousButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      splashColor: lightAccentColor,
-      highlightColor: Colors.red,
-      icon: Icon(
-        Icons.skip_previous,
-        color: Colors.white,
-        size: 35.0,
-      ),
-      onPressed: () {},
+    return AudioPlaylistComponent(
+      playlistBuilder: (context, Playlist playlist, Widget child) {
+        return IconButton(
+          splashColor: lightAccentColor,
+          highlightColor: Colors.red,
+          icon: Icon(
+            Icons.skip_previous,
+            color: Colors.white,
+            size: 35.0,
+          ),
+          onPressed: playlist.previous,
+        );
+      },
     );
   }
 }
@@ -112,15 +143,19 @@ class NextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      splashColor: lightAccentColor,
-      highlightColor: Colors.transparent,
-      icon: Icon(
-        Icons.skip_next,
-        color: Colors.white,
-        size: 35.0,
-      ),
-      onPressed: () {},
+    return AudioPlaylistComponent(
+      playlistBuilder: (context, Playlist playlist, child) {
+        return IconButton(
+          splashColor: lightAccentColor,
+          highlightColor: Colors.transparent,
+          icon: Icon(
+            Icons.skip_next,
+            color: Colors.white,
+            size: 35.0,
+          ),
+          onPressed: playlist.next,
+        );
+      },
     );
   }
 }
--- just_flutteringgggg/lib/part4_music_player/main_music_player.dart
+++ just_flutteringgggg/lib/part4_music_player/main_music_player.dart
@@ -4,6 +4,8 @@ import 'package:just_flutteringgggg/part4_music_player/songs.dart';
 import 'package:just_flutteringgggg/part4_music_player/bottom_controls.dart';
 import 'package:just_flutteringgggg/helper/radial_drag_gesture_detector.dart';
 
+import 'package:fluttery_audio/fluttery_audio.dart';
+
 import 'dart:math';
 
 const DEGREES_360 = pi * 2;
@@ -13,79 +15,209 @@ class MusicPlayer extends StatefulWidget {
 }
 
 class _MusicPlayerState extends State<MusicPlayer> {
-  double _seekPercent = 0.25;
-
-  void _onRadialDragStart(PolarCoord polarCoord) {}
-
-  void _onRadialDragUpdate(PolarCoord polarCoord) {}
-
-  void _onRadialDragEnd() {}
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () {},
-          color: const Color(0xFFDDDDDD),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
+    return AudioPlaylist(
+      // audioUrl: demoPlaylist.songs[1].audioUrl,
+      playlist: demoPlaylist.songs.map((DemoSong song) {
+        return song.audioUrl;
+      }).toList(),
+      playbackState: PlaybackState.playing,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
             onPressed: () {},
             color: const Color(0xFFDDDDDD),
           ),
-        ],
-        title: Text(''),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+              color: const Color(0xFFDDDDDD),
+            ),
+          ],
+          title: Text(''),
+        ),
+        body: Column(children: <Widget>[
+          /** Seekbar*/
+          Expanded(
+            child: AudioPlaylistComponent(
+              playlistBuilder: (context, Playlist playlist, child) {
+                String albumArtUrl =
+                    demoPlaylist.songs[playlist.activeIndex].albumArtUrl;
+                return new AudioRadialSeekBar(
+                  albumArtUrl: albumArtUrl,
+                );
+              },
+            ),
+          ),
+
+          /** Visualizer*/
+          Container(
+            width: double.infinity,
+            height: 125.0,
+          ),
+
+          /** Song title, artist name and controls*/
+          BottomControls()
+        ]),
       ),
-      body: Column(children: <Widget>[
-        // Seekbar
-        Expanded(
-          child: RadialDragGestureDetector(
-            onRadialDragStart: _onRadialDragStart,
-            onRadialDragUpdate: _onRadialDragUpdate,
-            onRadialDragEnd: _onRadialDragEnd,
+    );
+  }
+}
+
+class AudioRadialSeekBar extends StatefulWidget {
+  final String albumArtUrl;
+  const AudioRadialSeekBar({Key key, this.albumArtUrl}) : super(key: key);
+
+  @override
+  AudioRadialSeekBarState createState() {
+    return new AudioRadialSeekBarState();
+  }
+}
+
+class AudioRadialSeekBarState extends State<AudioRadialSeekBar> {
+  double _seekPercent;
+  @override
+  Widget build(BuildContext context) {
+    return AudioComponent(
+      updateMe: [
+        WatchableAudioProperties.audioPlayhead,
+        WatchableAudioProperties.audioSeeking,
+        /** Start rotating animation on buffering */
+      ],
+      playerBuilder: (BuildContext context, AudioPlayer player, Widget child) {
+        double playbackProgress = 0.0;
+        if (player.audioLength != null && player.position != null
+            /**When buffering, when you don't know the size of media, or liveStream(but it doesn't make much sense) */) {
+          playbackProgress = player.position.inMilliseconds /
+              player.audioLength.inMilliseconds;
+        }
+
+        _seekPercent = player.isSeeking ? _seekPercent : null;
+        return RadialSeekBar(
+          progress: playbackProgress,
+          seekPercent: _seekPercent,
+          onSeekRequested: (double seekPercentage) {
+            _seekPercent = seekPercentage;
+            final seekMilliSeconds =
+                player.audioLength.inMilliseconds * seekPercentage;
+            Duration seekDuration =
+                Duration(milliseconds: seekMilliSeconds.round());
+            player.seek(seekDuration);
+            setState(() {});
+          },
+          child: Container(
+            color: accentColor,
+            child: Image.network(
+              widget.albumArtUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class RadialSeekBar extends StatefulWidget {
+  final double seekPercent;
+  final double progress;
+  final Widget child;
+  final Function(double) onSeekRequested; //When user lets go of dragging
+  RadialSeekBar({
+    this.seekPercent,
+    this.progress,
+    this.onSeekRequested,
+    this.child,
+  });
+  _RadialSeekBarState createState() => _RadialSeekBarState();
+}
+
+class _RadialSeekBarState extends State<RadialSeekBar> {
+  double _progress = 0.0;
+  PolarCoord _startDragCoord;
+  double _startDragPecentage;
+  double _currentDragPercent;
+
+  void initState() {
+    super.initState();
+    _progress = widget.progress;
+  }
+
+  @override
+  void didUpdateWidget(RadialSeekBar oldWidget) {
+    /**
+     * When we are not dragging, the seekPercent will change automatically
+     * But if we are dragging then we are only listening to currentDragPercent
+     * and even though widget.seekPercent is changing beneath us, we won't be listening to it. 
+     * But now we will.
+     */
+    super.didUpdateWidget(oldWidget);
+    _progress = widget.progress;
+  }
+
+  void _onRadialDragStart(PolarCoord polarCoord) {
+    _startDragCoord = polarCoord;
+    _startDragPecentage = _progress;
+  }
+
+  void _onRadialDragUpdate(PolarCoord polarCoord) {
+    var difference = polarCoord.angle - _startDragCoord.angle;
+    _currentDragPercent =
+        ((difference / DEGREES_360) + _startDragPecentage) % 1;
+    setState(() {});
+
+    //print(currentDragPercent);
+  }
+
+  void _onRadialDragEnd() {
+    if (widget.onSeekRequested != null) {
+      widget.onSeekRequested(_currentDragPercent);
+    }
+    _startDragCoord = null;
+    _startDragPecentage = null;
+    _currentDragPercent = null;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double thumbPosition =
+        _currentDragPercent ?? (widget.seekPercent ?? _progress);
+    return Container(
+      child: RadialDragGestureDetector(
+        onRadialDragStart: _onRadialDragStart,
+        onRadialDragUpdate: _onRadialDragUpdate,
+        onRadialDragEnd: _onRadialDragEnd,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors
+              .transparent /** In absence of color, touch events are not processed */,
+          child: Center(
             child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors
-                  .transparent /** In absence of color, touch events are not processed */,
-              child: Center(
-                child: Container(
-                  width: 150.0,
-                  height: 150.0,
-                  child: RadialProgressBar(
-                    progressColor: accentColor,
-                    trackColor: const Color(0xFFDDDDDD),
-                    thumbColor: lightAccentColor,
-                    progressPercentage: _seekPercent,
-                    thumbPosition: _seekPercent,
-                    innerPadding: 10.0,
-                    child: ClipOval(
-                      clipper: CircleClipper(),
-                      child: Image.network(
-                        demoPlaylist.songs[1].albumArtUrl,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
+              width: 150.0,
+              height: 150.0,
+              child: RadialProgressBar(
+                progressColor: accentColor,
+                trackColor: const Color(0xFFDDDDDD),
+                thumbColor: lightAccentColor,
+                progressPercentage: thumbPosition,
+                thumbPosition: thumbPosition,
+                innerPadding: 10.0,
+                child: ClipOval(
+                  clipper: CircleClipper(),
+                  child: widget.child,
                 ),
               ),
             ),
           ),
         ),
-        // Visualizer
-        Container(
-          width: double.infinity,
-          height: 125.0,
-        ),
-
-        // Song title, artist name and controls
-        BottomControls()
-      ]),
+      ),
     );
   }
 }
